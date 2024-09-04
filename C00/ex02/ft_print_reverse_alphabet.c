721196194028babd40d0dc7936c0070d16370e6a
Andrielly Dias <andrielly.12dias.ad@gmail.com>
2024-09-04 16:40:17 +0100

    ft_print_reverse_alphabet
    ft_print_reverse_alphabet

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adias-do <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/02 20:46:52 by adias-do          #+#    #+#             */
+/*   Updated: 2024/08/06 16:36:48 by adias-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letra;
+
+	letra = 'z';
+	while (letra >= 'a')
+	{
+		write (1, &letra, 1);
+		letra--;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}*/
