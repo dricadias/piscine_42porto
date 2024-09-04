8927cef0ae9198eb6739e3b43444bb6f027308d9
Andrielly Dias <andrielly.12dias.ad@gmail.com>
2024-09-04 16:39:42 +0100

    ft_print_alphabet
    ft_print_alphabet

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adias-do <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/01 15:28:39 by adias-do          #+#    #+#             */
+/*   Updated: 2024/08/06 16:33:01 by adias-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letra;
+
+	letra = 'a';
+	while (letra <= 'z')
+	{
+		write(1, &letra, 1);
+		letra++;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}*/
