36bc6fd7c56656f5a9462dc048c23ea5d2981ad3
Andrielly Dias <andrielly.12dias.ad@gmail.com>
2024-09-06 18:16:08 +0100

    ft_ultimate_ft
    ft_ultimate_ft

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adias-do <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 18:49:35 by adias-do          #+#    #+#             */
+/*   Updated: 2024/08/08 11:32:25 by adias-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+/*#include <stdio.h>*/
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int n;
+	int *nbr9; 
+ 	int **nbr8;
+ 	int ***nbr7;
+ 	int ****nbr6;
+ 	int *****nbr5;
+ 	int ******nbr4;
+ 	int *******nbr3;
+ 	int ********nbr2;
+ 	int *********nbr;
+	
+	n = 4;
+	nbr9 = &n;
+	nbr8 = &nbr9;
+	nbr7 = &nbr8;
+	nbr6 = &nbr7;
+	nbr5 = &nbr6;
+	nbr4 = &nbr5;
+	nbr3 = &nbr4;
+	nbr2 = &nbr3;
+	nbr = &nbr2;
+	
+	ft_ultimate_ft(nbr);
+	printf("%d", *********nbr);
+	return (0);
+}*/
