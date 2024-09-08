d1ddfa06fe630ebb6e17eecb5e5ab9907d4fd293
Andrielly Dias <andrielly.12dias.ad@gmail.com>
2024-09-08 12:15:40 +0100

    ft_strncpy
    ft_strncpy

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adias-do <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 18:39:44 by adias-do          #+#    #+#             */
+/*   Updated: 2024/08/14 18:17:24 by adias-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>*/
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	c;
+
+	c = 0;
+	while (c < n && src[c] != '\0')
+	{
+		dest[c] = src[c];
+		c++;
+	}
+	while (c < n)
+	{
+		dest[c] = '\0';
+		c++;
+	}
+	return (dest);
+}
+
+/*int	main()
+{
+	char dest1[10];
+	char source[] = "hello";
+	unsigned int n = 3;
+	ft_strncpy(dest1, source, n);
+	printf("%s\n", dest1);
+	return (0);
+}*/
